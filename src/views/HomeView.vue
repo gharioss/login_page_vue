bf1e6490590edeea283ba860f46b2fb040a75f52
gharioss <njulie818@gmail.com>
2025-01-04 10:47:58 +0100

    Toolbar + add dependances and icons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <v-toolbar density="compact">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn variant="outlined" size="small" color="red">
+        Login
+      </v-btn>
+    </v-toolbar>
 </template>
